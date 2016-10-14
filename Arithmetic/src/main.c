29d8cd52f09c1575a0c129c4bd720d055cfcb825
Fanwd <fanweidong2014@163.com>
2016-10-14 23:38:50 +0800

    Revert "TwoForkTree"
    This reverts commit 344dcece8fe7da965c7180c439d81a9e775f784e.

--- Arithmetic/src/main.c
+++ Arithmetic/src/main.c
@@ -1,7 +1,6 @@
 #include "Global.h"
 #include "RateOfSimilar.h"
 #include "MergeSort.h"
-#include "TwoForkTree.h"
 
 void quick_test();
 void part_test();
@@ -13,8 +12,7 @@ void heap_sort_test();
 
 int main()
 {
-    TwoForkTreeTest();
-//    MergeSort_test();
+    MergeSort_test();
 //    RateOfSimilar_test();
 //    RateOfSimilar_c_test();
 //    printf("%d\n", (int)pow(10, 2*1));
